ac75450a93b00efb7f62b4496816ed0978c0cc10
LAB22_USER ON WS30 <L22X07W30@citulab.net>
2025-02-10 08:56:26 +0800

    commit1

---
+++ bituindestination/bituindestination/src/main/java/edu/cit/bituindestination/BituindestinationApplication.java
@@ -0,0 +1,14 @@
+package edu.cit.bituindestination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BituindestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BituindestinationApplication.class, args);
+	}
+	// uten dako
+
+}
---
+++ bituindestination/bituindestination/src/test/java/edu/cit/bituindestination/BituindestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.bituindestination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BituindestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
