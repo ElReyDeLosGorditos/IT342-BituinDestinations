2ac16e1e2c2287d1519bbb71d6161e7a5ed18dda
istroberiless <kyle.inion@gmail.com>
2025-03-17 08:52:10 +0800

    backend setup

---
+++ Backend/BituinDestinations/src/main/java/edu/cit/bituindestinations/BituinDestinationsApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.bituindestinations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BituinDestinationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BituinDestinationsApplication.class, args);
+    }
+
+}
---
+++ Backend/BituinDestinations/src/test/java/edu/cit/bituindestinations/BituinDestinationsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.bituindestinations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BituinDestinationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
