e360ab39992740964d9db39a7bfa971c84020469
istroberiless <kyle.inion@gmail.com>
2025-05-06 15:51:36 +0800

    uncomment   base: "/IT342-BituinDestinations/"

--- frontend-web/BituinFrontWeb/vite.config.js
+++ frontend-web/BituinFrontWeb/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
-
-  // "/IT342-BituinDestinations",
+  base: "/IT342-BituinDestinations/"
 })
