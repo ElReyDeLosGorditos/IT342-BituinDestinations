7bfcff7fdcb4c0952848ac2a422b11a7303a49c0
istroberiless <kyle.inion@gmail.com>
2025-05-06 16:17:06 +0800

    please

--- frontend-web/BituinFrontWeb/src/App.jsx
+++ frontend-web/BituinFrontWeb/src/App.jsx
@@ -20,7 +20,7 @@ import BookingReceipt from './pages/user/BookingReceipt.jsx';
 
 function App() {
   return (
-      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           {/* Public Routes */}
